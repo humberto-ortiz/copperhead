54bce0d62ffe95cbb74197a1da2e113ec157241f
Humberto Ortiz-Zuazaga <humberto@hpcf.upr.edu>
2014-10-18 11:12:19 -0400

    Added GPL license. Made function for random sequences.

--- test.jl
+++ test.jl
@@ -1,7 +1,16 @@
+# test.jl - toy Eulerian assembler in Julia
+# Copyright 2014 - Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
+# Distributed under the GNU General Public Licence v3. See LICENSE
+
+# Function to make a random nucleotide sequence of length N
+function randseq(n)
+  a = rand(1:4, n)
+  map((i) -> "acgt"[i], a)
+end
+
+# Main function
+
+# Initialize the random number generator to always make the same sequence
 srand(0)
 
-a = rand(1:4, 1000)
-
-seq = map((i) -> "acgt"[i], a)
-
-println(seq)
+println(randseq(100))
