a1361a690280532840a0a878a9aaff56893a8a4d
Humberto Ortiz-Zuazaga <humberto@hpcf.upr.edu>
2014-10-19 19:53:12 -0400

    Added function to pretty print sequences, one to generate random reads.

--- test.jl
+++ test.jl
@@ -8,9 +8,39 @@ function randseq(n)
   map((i) -> "acgt"[i], a)
 end
 
+# Print sequences (arrays of characters) nicely.
+# If passed optional name, print a fasta format header.
+function printseq(seq, name="")
+  count = 0
+  if ""!=name
+    print(">")
+    println(name)
+  end
+  for c in seq
+    print(c)
+    count = count + 1
+    if count == 65
+      println()
+      count = 0
+    end
+  end
+end
+
+# make a set of random reads from a sequence
+function makereads(seq, readlen, n)
+  stop = length(seq) - readlen
+  starts = rand(1:stop, n)
+  map((i) -> seq[i:i+readlen-1], starts)
+end
+
 # Main function
 
 # Initialize the random number generator to always make the same sequence
 srand(0)
 
-println(randseq(100))
+myseq = randseq(1000)
+#printseq(myseq, "myseq")
+
+reads = makereads(myseq, 15, 500)
+
+map((seq) -> begin printseq(seq); println() end, reads[1:10])
